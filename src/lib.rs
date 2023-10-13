43de5327531d6d2b0c90cd5b48e8b8051cff05bb
kevin-koshy <kevin_koshy@hotmail.com>
2023-10-12 22:27:32 -0400

    Adding Edit functionality

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub enum ToDoOption {
     Delete,
     Edit,
     Exit,
+    Display,
     }
 
 impl std::str::FromStr for ToDoOption {
@@ -18,6 +19,7 @@ impl std::str::FromStr for ToDoOption {
             "2" => Ok(ToDoOption::Delete),
             "3" => Ok(ToDoOption::Edit),
             "4" => Ok(ToDoOption::Exit),
+            "5" => Ok(ToDoOption::Display),
             _ => Err("Invalid option".to_string()),
         }
     }
@@ -42,9 +44,20 @@ pub fn match_task(task:ToDoOption) -> (){
         }
         ToDoOption::Edit => {
             println!("Enter the task to edit");
+            let mut task_edit = String::new();
+            io::stdin().read_line(&mut task_edit).expect("Failed to read line");
+            println!("Enter the new task");
+            let mut new_task = String::new();
+            io::stdin().read_line(&mut new_task).expect("Failed to read line");
+            edit_task(task_edit.trim().to_string(), new_task.trim().to_string());
+            println!("You entered:   {:?}", task_edit.trim());
         }
         ToDoOption::Exit => {
             println!("Exiting");
+        }
+        ToDoOption::Display => {
+            println!("Displaying");
+            display_tasks();
         }
     }
 }
@@ -54,7 +67,7 @@ fn create_connection(task: String) -> Result<Connection, rusqlite::Error> {
     conn.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, ITEMS TEXT NOT NULL)",(),)?;
     println!("Table created");
     conn.execute("INSERT INTO users (ITEMS) VALUES (?1)", [&task])?;
-    println!("Task added");
+    println!("Task adde3d");
     Ok(conn)
     }
 
@@ -62,5 +75,22 @@ fn delete_task(task:String) -> Result<(), rusqlite::Error>{
     let conn = Connection::open("todo.db")?;
     conn.execute("DELETE FROM users WHERE ITEMS = ?1", [task])?;
     println!("Task deleted");
+    Ok(())
+}
+
+fn edit_task(item_to_edit:String, new_task:String) -> Result<(), rusqlite::Error>{
+    let conn = Connection::open("todo.db")?;
+    conn.execute("UPDATE users SET ITEMS = ?1 WHERE ITEMS = ?2", [new_task, item_to_edit])?;
+    println!("Task edited");
+    Ok(())
+}
+
+fn display_tasks() -> Result<(), rusqlite::Error>{
+    let conn = Connection::open("todo.db")?;
+    let mut stmt = conn.prepare("SELECT * FROM users")?;
+    let mut rows = stmt.query([])?;
+    while let Some(row) = rows.next()? {
+        println!("{}: {}", row.get(0), row.get(1));
+    }
     Ok(())
 }
